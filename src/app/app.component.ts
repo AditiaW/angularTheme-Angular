503838897c7f6d841dadb3fd2bbd2f936cf4f1bf
Aditia Winanda <aditiaw1507@gmail.com>
2023-09-15 10:19:18 +0700

    commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  email = 'aditia.winanda15072002@gmail.com';
+  nama = 'Aditia Winanda';
+  nim = '202102310';
+  title = 'angularTheme';
+}
